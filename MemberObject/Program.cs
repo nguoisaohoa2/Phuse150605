4d17d78ff695a62009441d8bf9fc5c1be31b5775
LAPTOP-JDQTLJL4\ASUS <phutuduylanhdao@gmail.com>
2022-06-26 01:20:10 +0700

    first commit

---
+++ MemberObject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MemberObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
